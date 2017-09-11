b73bce41a174bd81a451609ee19a926c31b9b608
shaohy <shaohy.zj@qq.com>
2017-09-11 18:12:29 +0800

    update vimrc.bundles

--- trans.sh
+++ trans.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # encoding: utf-8
-cat systemd.service | sed -r -e 's@^@\tlet l = l + 1 | call append(l,"@g;
-                    s@$@")@g;
-                    1i nnoremap <C-m>s :call
-                    SystemdMenu()<CR>\nfunction
-                    SystemdMenu()\nlet l = line(".") - 1
-                    $a endfunction
-                    '
+cat systemd.service | sed -e '
+    s@^@\tlet l = l + 1 | call append(l,"@g;
+    s@$@")@g;
+    1i nnoremap <C-m>s :call SystemdMenu()<CR>\nfunction SystemdMenu()\n\tlet l = line(".") - 1;
+    $a endfunction
+'
