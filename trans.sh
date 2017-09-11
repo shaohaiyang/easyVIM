7a90a00e7351ed03f5815bb112eb44ed07a37dae
shaohy <shaohy.zj@qq.com>
2017-09-11 18:17:34 +0800

    update vimrc.bundles

--- trans.sh
+++ trans.sh
@@ -3,6 +3,6 @@
 cat systemd.service | sed -e '
     s@^@\tlet l = l + 1 | call append(l,"@g;
     s@$@")@g;
-    1i nnoremap <C-m>s :call SystemdMenu()<CR>\nfunction SystemdMenu()\n\tlet l = line(".") - 1;
+    1i nnoremap <C-m>s :call SystemdMenu()<CR>\nfunction SystemdMenu()\n\tlet l = line(".") - 1
     $a endfunction
 '
